136cc03cf6b6208b87bec855cb2b0f6dc318393a
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-07-15 15:53:11 -0500

    Update function to align with hugheylab styling guides and add comments

--- R/get_pubmed_xml.R
+++ R/get_pubmed_xml.R
@@ -1,27 +1,36 @@
+# This function grabs and downloads all files from the FTP server to the data subfolder.
 
-getPubmedXml = function(startFile = 1, maxFiles = NULL, skipBaseline = FALSE, skipUpdates = FALSE){
-  urlBaseline = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/"
-  urlUpdates = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/"
+getPubmedXml = function(startFile = 1, # Which indexed file number to start with downloading (compared against i)
+                        lastFile = NULL, # Which indexed file number to stop downloading at
+                        skipBaseline = FALSE, # Indicates if you wish to skip the baseline downloads all together
+                        skipUpdates = FALSE # Indicates if you wish to skip the update downloads all together
+                        ){
+
+  # Set the url variables to the FTP server folder links
+  urlBaseline = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/'
+  urlUpdates = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/'
+
+  # Returns back the file list for both URLs and writes them to their respective .txt files
   baseline = RCurl::getURL(url = urlBaseline)
-  write(baseline, file = file.path("data", "baseline.txt"))
+  write(baseline, file = file.path('data', 'baseline.txt'))
   updates = RCurl::getURL(url = urlUpdates)
-  write(updates, file = file.path("data", "baseline.txt"))
-  baselineFileList = stringr::str_extract_all(baseline, pattern = "pubmed20n.*\\.gz(?=\n)")
-  updateFileList = stringr::str_extract_all(updates, pattern = "pubmed20n.*\\.gz(?=\n)")
-  for(baselineFile in baselineFileList[[1]]){
-    if((is.null(maxFiles) || i <= maxFiles) && !skipBaseline){
-      if(!file.exists(file.path("data", baselineFile))){
-        download.file(str_c(urlBaseline, baselineFile), file.path("data", baselineFile))
-      }
-    }
-    i = i + 1
-  }
-  for(updateFile in updateFileList[[1]]){
-    if((is.null(maxFiles) || i <= maxFiles) && !skipUpdates){
-      if(!file.exists(file.path("data", updateFile))){
-        download.file(str_c(urlUpdates, updateFile), file.path("data", updateFile))
-      }
-    }
-    i = i + 1
-  }
+  write(updates, file = file.path('data', 'updates.txt'))
+
+  # Extracts a string list of file names via regex
+  baselineFileList = stringr::str_extract_all(baseline, pattern = 'pubmed20n.*\\.gz(?=\n)')
+  updateFileList = stringr::str_extract_all(updates, pattern = 'pubmed20n.*\\.gz(?=\n)')
+
+  # Iterates over baseline files and checks to decide to download or not
+  for (baselineFile in baselineFileList[[1]]){
+    if ((is.null(startFile) || i >= startFile) && (is.null(lastFile) || i <= lastFile) && !skipBaseline){
+      if (!file.exists(file.path('data', baselineFile))){
+        download.file(str_c(urlBaseline, baselineFile), file.path('data', baselineFile))}}
+    i = i + 1}
+
+  # Iterates over update files and checks to decide to download or not
+  for (updateFile in updateFileList[[1]]){
+    if ((is.null(startFile) || i >= startFile) && (is.null(lastFile) || i <= lastFile) && !skipUpdates){
+      if (!file.exists(file.path('data', updateFile))){
+        download.file(str_c(urlUpdates, updateFile), file.path('data', updateFile))}}
+    i = i + 1}
 }
