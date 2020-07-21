39bc6d12cdc0bc34035a63cae89680609be5a2db
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-07-21 11:18:09 -0500

    Add MD5 Checksum Checking
    Add checkSumCompare function for validating checksum of a file against it's downloaded checksum and incorporated into logic of getPubmedXml function

--- R/get_pubmed_xml.R
+++ R/get_pubmed_xml.R
@@ -1,3 +1,22 @@
+
+#' @export
+# This function compares the checksum of 2 files in a directory.
+checkSumCompare = function(dataDir, # Directory the files are located in
+                           md5File, # File to use to read downloaded MD5 sum
+                           baseFile = NULL # File to calculate and compare against, if null uses same file name and path as md5File but removes '.md5' from end of filename
+){
+  if(is.null(baseFile) || baseFile == ''){
+    baseFile = sub('.md5','', md5File)}
+
+  md5Calculated = tools::md5sum(file.path(dataDir, baseFile))
+  md5Downloaded = readr::read_file(file.path(dataDir, md5File))
+  md5Downloaded = sub('.*= ', '', md5Downloaded)
+  md5Downloaded = sub('\n', '', md5Downloaded)
+
+  return(md5Downloaded == md5Calculated[[1]])
+}
+
+
 # This function grabs and downloads all files from the FTP server to the data subfolder.
 #' @export
 getPubmedXml = function(startFile = 1, # Which indexed file number to start with downloading (compared against i)
@@ -18,21 +37,28 @@ getPubmedXml = function(startFile = 1, # Which indexed file number to start with
   write(updates, file = file.path(dataDir, 'updates.txt'))
 
   # Extracts a string list of file names via regex
-  baselineFileList = stringr::str_extract_all(baseline, pattern = 'pubmed20n.*\\.gz(?=\n)')
-  updateFileList = stringr::str_extract_all(updates, pattern = 'pubmed20n.*\\.gz(?=\n)')
+  baselineFileList = stringr::str_extract_all(baseline, pattern = 'pubmed20n.*\\.gz.*(?=\n)')
+  updateFileList = stringr::str_extract_all(updates, pattern = 'pubmed20n.*\\.gz.*(?=\n)')
   i = 1
 
   # Iterates over baseline files and checks to decide to download or not
   for (baselineFile in baselineFileList[[1]]){
     if ((is.null(startFile) || i >= startFile) && (is.null(lastFile) || i <= lastFile) && !skipBaseline){
       if (!file.exists(file.path(dataDir, baselineFile))){
-        download.file(stringr::str_c(urlBaseline, baselineFile), file.path(dataDir, baselineFile))}}
-    i = i + 1}
+        download.file(stringr::str_c(urlBaseline, baselineFile), file.path(dataDir, baselineFile))}
+        if(!endsWith(baselineFile, '.xml.gz')){
+          compare = checkSumCompare(dataDir, baselineFile)
+          if(!compare){
+            'COMPARE NOT TRUE FOR MD5 SUM'}}}
+    if(!endsWith(baselineFile, '.xml.gz')){
+      i = i + 1}}
 
   # Iterates over update files and checks to decide to download or not
   for (updateFile in updateFileList[[1]]){
     if ((is.null(startFile) || i >= startFile) && (is.null(lastFile) || i <= lastFile) && !skipUpdates){
       if (!file.exists(file.path(dataDir, updateFile))){
         download.file(stringr::str_c(urlUpdates, updateFile), file.path(dataDir, updateFile))}}
-    i = i + 1}
+    if(endsWith(updateFile, '.xml.gz')){
+      i = i + 1}}
+
 }
