66e26da665553f2757cb78fbdafda411e23ad009
Jake Hughey <jakejhughey@gmail.com>
2020-07-30 15:22:59 -0500

    added documentation and improved logic in the getCitation function.

--- R/get_citation.R
+++ R/get_citation.R
@@ -15,6 +15,32 @@ getCitationInfo = function(filename = 'open_citation_collection.zip',
   return(citationInfo)}
 
 
+#' Get public-domain citation data
+#'
+#' Get the latest version of the NIH Open Citation Collection from figshare
+#' [here](https://nih.figshare.com/collections/iCite_Database_Snapshots_NIH_Open_Citation_Collection_/4586573),
+#' and optionally write it to the database. This function requires the shell
+#' command `unzip`, available by default on most Unix systems.
+#'
+#' @param localDir String indicating path to directory containing the citation
+#'   file or to which the citation file should be downloaded.
+#' @param filename String indicating name of the citation file. This should not
+#'   normally be changed from the default.
+#' @param nrows Number indicating how many rows of the citation file to read.
+#'   This should not normally be changed from the default.
+#' @param tableSuffix String indicating suffix, if any, to append to the table
+#'   name.
+#' @param overwrite Logical indicating whether to overwrite an existing citation
+#'   file and existing citation table in the database.
+#' @param dbname String indicating name of the database, if any, in which to
+#'   create the citation table.
+#' @param ... Other arguments passed to [DBI::dbConnect()].
+#'
+#' @return Normally, a data.table with columns `citing_pmid` and `cited_pmid`.
+#'   Beware this is a large table and could swamp some machines' memories. If
+#'   `dbname` is not `NULL` and the existing citation table and the citation
+#'   file have identical MD5 sums, then the function returns `NULL` invisibly.
+#'
 #' @export
 getCitation = function(localDir, filename = 'open_citation_collection.zip',
                        nrows = Inf, tableSuffix = NULL, overwrite = FALSE,
@@ -22,26 +48,38 @@ getCitation = function(localDir, filename = 'open_citation_collection.zip',
 
   filepath = file.path(localDir, filename)
   tableBase = 'citation'
+  citationName = paste_(tableBase, tableSuffix)
+  versionName = paste_(tableBase, 'version', tableSuffix)
+
+  if (is.null(dbname)) {
+    con = NULL
+    md5Database = ''
+  } else {
+    con = DBI::dbConnect(RPostgres::Postgres(), dbname = dbname, ...)
+    if (DBI::dbExistsTable(con, versionName)) {
+      dVersion = DBI::dbReadTable(con, versionName)
+      md5Database = dVersion$md5_computed
+    } else {
+      md5Database = ''}}
 
   if (!file.exists(filepath) || isTRUE(overwrite)) {
     citationInfo = getCitationInfo()
 
-    if (!is.null(dbname)) {
-      con = DBI::dbConnect(RPostgres::Postgres(), dbname = dbname, ...)
-      tableName = paste_(tableBase, 'version', tableSuffix)
-
-      if (DBI::dbExistsTable(con, tableName)) {
-        dVersion = DBI::dbReadTable(con, tableName)
-
-        if (dVersion$md5_computed == citationInfo$supplied_md5) {
-          message('Citation table is already up-to-date.')
-          return()}}}
+    if (citationInfo$supplied_md5 == md5Database) {
+      message('Citation table in database is already up-to-date.')
+      return(invisible())}
 
     utils::download.file(citationInfo$download_url, filepath, mode = 'wb')
     md5Computed = tools::md5sum(filepath)
 
     if (md5Computed != citationInfo$supplied_md5) {
-      stop('Supplied and computed MD5 checksums do not match.')}}
+      stop('Supplied and computed MD5 checksums do not match.')}
+
+  } else {
+    md5Computed = tools::md5sum(filepath)
+    if (md5Computed == md5Database) {
+      message('Citation table in database is already up-to-date.')
+      return(invisible())}}
 
   # read the file
   if (tools::file_ext(filepath) == 'zip') {
@@ -50,25 +88,19 @@ getCitation = function(localDir, filename = 'open_citation_collection.zip',
   } else {
     dCitation = data.table::fread(filepath, nrows = nrows)}
 
-  # check the file
+  # check the table
   stopifnot(all.equal(colnames(dCitation), c('citing', 'referenced')),
             all(sapply(dCitation, class) == 'integer'))
   setnames(dCitation, c('citing_pmid', 'cited_pmid'))
 
   # send to db
-  if (!is.null(dbname)) {
-    con = DBI::dbConnect(RPostgres::Postgres(), dbname = dbname, ...)
-    DBI::dbWriteTable(con, paste_(tableBase, tableSuffix), dCitation,
-                      overwrite = overwrite)
-
-    if (!exists('md5Computed')) {
-      md5Computed = tools::md5sum(filepath)}
+  if (!is.null(con)) {
+    DBI::dbWriteTable(con, citationName, dCitation, overwrite = overwrite)
 
     dVersion = data.table(
       md5_computed = md5Computed, datetime_processed = Sys.time())
 
     # if we make it to this point, set overwrite to TRUE
-    DBI::dbWriteTable(con, paste_(tableBase, 'version', tableSuffix),
-                      dVersion, overwrite = TRUE)}
+    DBI::dbWriteTable(con, versionName, dVersion, overwrite = TRUE)}
 
   return(dCitation)}
