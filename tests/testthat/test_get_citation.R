546ce9b5c7ec7e12b6cc5461f7765c9ac9851812
Jake Hughey <jakejhughey@gmail.com>
2020-08-21 15:40:51 -0500

    added and revised tests.

--- tests/testthat/test_get_citation.R
+++ tests/testthat/test_get_citation.R
@@ -1,22 +1,19 @@
 refDir = 'citation_info'
 
 test_that('getCitationInfo', {
-  filenameCols = 'citation_info_columns.rds'
-
-  citationInfoObs = getCitationInfo()
-  citationInfoColsExp = readRDS(file.path(refDir, filenameCols))
-
-  expect_s3_class(citationInfoObs, 'data.table')
-  expect_equal(citationInfoColsExp, colnames(citationInfoObs))
-  expect_equal(1, nrow(citationInfoObs))
+  citationInfo = getCitationInfo()
+  colsExp = c('is_link_only', 'name', 'supplied_md5',
+              'computed_md5', 'id', 'download_url', 'size')
+  expect_s3_class(citationInfo, 'data.table')
+  expect_equal(colnames(citationInfo), colsExp)
+  expect_equal(nrow(citationInfo), 1L)
 })
 
 test_that('getCitation', {
   filename = 'open_citation_collection.zip'
   filenameExp = 'citation_info_collection.rds'
-
-  dCitationObs = getCitation(localDir = refDir, filename = filename, nrows = 50, checkMd5 = FALSE)
+  dCitationObs = getCitation(
+    localDir = refDir, filename = filename, nrows = 50L, checkMd5 = FALSE)
   dCitationExp = readRDS(file.path(refDir, filenameExp))
-
-  expect_equal(dCitationExp, dCitationObs, check.attributes = FALSE)
+  expect_equal(dCitationObs, dCitationExp)
 })
---
+++ tests/testthat/test_utilities.R
@@ -0,0 +1,35 @@
+logPath = 'cutter.log'
+local_file(logPath)
+
+dStart = data.table(xml_filename = 'all', step = 'start', status = 0)
+writeLogFile(logPath, dStart, append = FALSE)
+writeLogFile(logPath, data.table('freedom.xml.gz', 'grant', 1))
+
+test_that('writeLogFile', {
+  dLog = data.table::fread(logPath, sep = '\t', na.strings = '')
+  expect_equal(colnames(dLog), c('datetime', colnames(dStart)))
+  expect_equal(nrow(dLog), 2L)
+  expect_null(writeLogFile(NULL, 'qwfp'))
+})
+
+test_that('getFailed', {
+  dFailed = getFailed(logPath)
+  expect_equal(colnames(dFailed), c('datetime', colnames(dStart)))
+  expect_equal(nrow(dFailed), 1L)
+})
+
+test_that('getMissing', {
+  refDir = 'pubmed_sample'
+  dbname = 'pmdb_sample_update.db'
+  dbtype = 'sqlite'
+  con = connect(dbtype, file.path(refDir, dbname))
+  dFile = data.table(xml_filename = c('pubmed20n1016.xml.gz', 'freedom.xml.gz'))
+  dFailed = getMissing(con = con, tableSuffix = NULL, dFile = dFile)
+  expect_equal(dFailed, dFile[.N])
+})
+
+test_that('getPgParams', {
+  pg = getPgParams('pgpass')
+  expect_s3_class(pg, 'data.table')
+  expect_equal(nrow(pg), 2L)
+})
