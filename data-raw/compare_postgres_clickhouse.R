05be1cd65990e6710d8a095e800de32018a2dbd0
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-09-30 10:43:05 -0500

    Add comparison script for postgres and clickhouse

---
+++ data-raw/compare_postgres_clickhouse.R
@@ -0,0 +1,33 @@
+library('pmparser')
+library('data.table')
+tables = c(names(pmparser:::getParsingTables('')), 'citation')
+conP =  pmparser:::connect('postgres', 'pmdbclick')
+conC =  pmparser:::connect('clickhouse', 'clicktest')
+for(table in tables){
+  q = glue('select * from {table}')
+  dtP = as.data.table(DBI::dbReadTable(conP, table))
+  for(colname in colnames(dtP)){
+    if(inherits(dtP[[colname]], 'Date')){
+      dtP[[colname]] = NULL
+    }
+  }
+  data.table::setorder(dtP)
+
+  dtC = as.data.table(DBI::dbReadTable(conC, table))
+  for(colname in colnames(dtC)){
+    if(inherits(dtC[[colname]], 'Date')){
+      dtC[[colname]] = NULL
+    }
+  }
+  data.table::setorder(dtC)
+
+  allEq = all.equal(dtP, dtC, check.attributes = FALSE)
+
+  if(isTRUE(allEq)){
+    warning(glue('{table} is equal.\n'))
+  } else{
+    warning(glue('{table} is not equal: \n{allEq}\n'))
+  }
+}
+pmparser:::disconnect(conP)
+pmparser:::disconnect(conC)
