5a028242ae8a9073c0bff8dea155086991b15b4d
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-10-27 15:33:39 -0500

    Create postgres bigquery comparison script

---
+++ data-raw/compare_postgres_bigquery.R
@@ -0,0 +1,25 @@
+library('pmparser')
+library('data.table')
+tables = c(names(pmparser:::getParsingTables('')), 'citation', 'citation_version')
+conP =  pmparser:::connect('postgres', 'pmdbclick')
+conB =  pmparser:::connect('bigquery', 'pmparser-test', project = 'pmparser-test', dataset = 'pmparser')
+for(table in tables){
+  q = glue('select * from {table}')
+  dtP = as.data.table(DBI::dbReadTable(conP, table))
+  data.table::setorder(dtP)
+
+  dtB = as.data.table(DBI::dbReadTable(conB, table))
+  data.table::setorder(dtB)
+
+  allEq = all.equal(dtP, dtB, check.attributes = FALSE)
+
+  if(isTRUE(allEq)){
+    warning(glue('{table} is equal.\n'))
+  } else{
+    warning(glue('{table} is not equal: \n{allEq}\n'))
+  }
+}
+warnings()
+pmparser:::disconnect(conP)
+pmparser:::disconnect(conB)
+
