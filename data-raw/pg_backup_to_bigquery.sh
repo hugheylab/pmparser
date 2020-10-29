5bcedb9b6e6a90c3420c25fea7d1d06cbefde6df
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-10-29 10:50:05 -0500

    Update script to create tables in database

--- data-raw/pg_backup_to_bigquery.sh
+++ data-raw/pg_backup_to_bigquery.sh
@@ -1,13 +1,21 @@
 SCHEMA="public"
 DB="pmdbclick"
 
+Rscript \
+  -e "pmparser:::createParsingTables(dbtype = 'bigquery', dbname = 'pmparser-test', project = 'pmparser-test, dataset = 'pmparser')" \
+  -e "bqCon = pmparser:::connect('bigquery', dbname = 'pmparser-test', project = 'pmparser-test, dataset = 'pmparser')" \
+  -e "pmparser:::dropPmidVersionColumn('', bqCon)" \
+  -e "pmparser:::disconnect(bqCon)"
+
+
 psql -Atc "select tablename from pg_tables where schemaname='$SCHEMA'" $DB |\
   while read TBL; do
-    psql -c "COPY $SCHEMA.$TBL TO STDOUT WITH (FORMAT CSV, HEADER)" $DB > $TBL.csv
+    psql -c "COPY $SCHEMA.$TBL TO STDOUT WITH (FORMAT CSV, HEADER, ENCODING 'UTF-8')" $DB > $TBL.csv
   done
 for f in *.csv; do
   tName=$(echo $f | sed 's/\.csv//g')
   bq load \
+  --allow_quoted_newlines \
   --autodetect \
   --source_format=CSV \
   "pmparser-test:pmparser.$tName" \
