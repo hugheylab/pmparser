cefc5c66ceaccb5c7c88bfabbc9135bde7863b9d
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-11-12 12:37:24 -0600

    Disconnect

--- data-raw/create_bigquery_from_postgres.R
+++ data-raw/create_bigquery_from_postgres.R
@@ -5,4 +5,5 @@ library(glue)
 createBigQueryFromPostgres = function(bqDbName = 'pmdbclick', project = 'pmparser-test', dataset = 'pmparser', tableName, nRowsPerChunk = 15000L){
   bqCon = pmparser:::connect('bigquery', bqDbName, project = 'pmparser-test', dataset = 'pmparser')
   pmparser:::writeTableInChunks(glue('{tableName}.csv'), bqCon, nRowsPerChunk, overwrite=TRUE, tableName)
+  pmparser:::disconnect(bqCon)
 }
