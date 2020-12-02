0769d00bc45a1178ec5dd61d125fad3589bc362c
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-12-02 12:05:47 -0600

    Add checkAndFix to iterate over chunks, compare, then fix

--- data-raw/create_bigquery_from_postgres.R
+++ data-raw/create_bigquery_from_postgres.R
@@ -5,7 +5,7 @@ library(bigrquery)
 library(doParallel)
 library(foreach)
 
-createBigQueryFromPostgres = function(pgDbName = 'pmdb', project = 'pmparser-test', dataset = 'pmdb', chunkSize = 15000L, overwriteTable = FALSE, fName = 'create_bigquery_result.csv'){
+createBigQueryFromPostgres = function(pgDbName = 'pmdb', project = 'pmparser-test', dataset = 'pmdb', chunkSize = 15000L, overwriteTable = FALSE, fName = 'create_bigquery_result.csv', checkAndFix = FALSE){
 
   # chunkSize variable is either an integer or a named list where the name is the table name and the value is the chunk size you want to process.
   # Integer values will be the chunkSize for all tables, and a named list will only process the tables in the list in the size provided.
@@ -69,11 +69,20 @@ createBigQueryFromPostgres = function(pgDbName = 'pmdb', project = 'pmparser-tes
       off = rowOff * tChunkSize
       dTable = data.table::as.data.table(DBI::dbGetQuery(pCon, glue('SELECT * FROM {`tableName`} ORDER BY {`colOrder`} LIMIT {`tChunkSize`} OFFSET {`off`}')))
 
+      if(is.TRUE(checkAndFix)){
+        dTableBQ = data.table::as.data.table(bq_dataset_query(bq_dataset(project, dataset), glue('SELECT * FROM {`tableName`} ORDER BY {`colOrder`} LIMIT {`tChunkSize`} OFFSET {`off`}')))
+
+        dTable = data.table::fsetdiff(dTable, dTableBQ)
+
+      }
+
+      if(nrow(dTable) > 0){
       # Append to BigQuery DB
       tryCatch({bq_table_upload(bq_table(project, dataset, tableName), values = dTable, write_disposition = 'WRITE_APPEND')},
                error = function(e){
                  dtFail = rbind(dtFail, data.table(table = tableName, offset = off, chunk_size =chunkSize, fail_reason = trimws(as.character(e)), timestamp = Sys.time()))
                })
+      }
 
     }
     pmparser:::disconnect(pCon)}
@@ -83,12 +92,23 @@ createBigQueryFromPostgres = function(pgDbName = 'pmdb', project = 'pmparser-tes
   return(dtFail)
 }
 
+
+
 retryBigQueryFromPostgres = function(dtFail, pgDbName = 'pmdb', project = 'pmparser-test', dataset = 'pmdb'){
   dtFail[,retryMsg := queryFun(table, chunk_size, offset, pgDbName, project, dataset)]
 }
 
 queryFun = function(table, chunkSize, off, pgDbName = 'pmdb', project = 'pmparser-test', dataset = 'pmdb'){
-  dTable = data.table::as.data.table(DBI::dbGetQuery(pCon, glue('SELECT * FROM {`tableName`} ORDER BY {`colOrder`} LIMIT {`tChunkSize`} OFFSET {`off`}')))
+  parseNames = pmparser:::getParsingTables('')
+  parseNames = c(parseNames, list(citation = data.table::data.table(citing_pmid = as.integer(), cited_pmid = as.integer()),
+                                  citation_version = data.table::data.table(md5_computed = as.character(), pmparser_version = as.character(), datetime_processed = as.POSIXct(as.character()))))
+
+  verExclude = c('pmid_status', 'xml_processed', 'citation', 'citation_version')
+  colNamesDT = parseNames[[table]]
+  if(!(tableName %in% verExclude)) colNamesDT[,version := NULL]
+  colOrder = paste(colnames(colNamesDT), collapse=', ')
+
+  dTable = data.table::as.data.table(DBI::dbGetQuery(pCon, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
 
   retMsg = 'Success'
   # Append to BigQuery DB
