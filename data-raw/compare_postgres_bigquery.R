b0faa0f0c41652bb91416f32ef31236b513751f7
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-12-09 13:37:01 -0600

    Partial testing using count, first, and last chunks

--- data-raw/compare_postgres_bigquery.R
+++ data-raw/compare_postgres_bigquery.R
@@ -12,10 +12,11 @@ project = 'pmparser-test'
 dataset = 'pmparser'
 conB =  pmparser:::connect('bigquery', 'pmparser-test', project = project, dataset = dataset)
 chunkSize = NULL
+complete = FALSE
 verExclude = c('pmid_status', 'xml_processed', 'citation', 'citation_version')
 notEqTables = data.table(table = as.character(), reason = as.character(), offset = as.integer(), chunkSize = as.integer())
-for(table in tables){
-  if(is.null(chunkSize)){
+for (table in tables) {
+  if (is.null(chunkSize)) {
     dtP = as.data.table(dbReadTable(conP, table))
     setorder(dtP)
 
@@ -24,15 +25,15 @@ for(table in tables){
 
     allEq = all.equal(dtP, dtB, check.attributes = FALSE)
 
-    if(isTRUE(allEq)){
+    if (isTRUE(allEq)) {
       warning(glue('{table} is equal.\n'))
-    } else{
+    } else {
       notEqTables = rbind(notEqTables, data.table(table = table, reason = allEq, offset = as.integer(NA), chunkSize = as.integer(NA)))
       warning(glue('{table} is not equal: \n{allEq}\n'))
     }
   } else {
-    colNamesDT = parseNames[[tableName]]
-    if(!(table %in% verExclude)) colNamesDT[,version := NULL]
+    colNamesDT = parseNames[[table]]
+    if (!(table %in% verExclude)) colNamesDT[,version := NULL]
     colOrder = paste(colnames(colNamesDT), collapse=', ')
 
     dCount = DBI::dbGetQuery(conP, glue('SELECT count(*) as count FROM {`table`}'))
@@ -40,29 +41,60 @@ for(table in tables){
 
     allEqTot = TRUE
 
-    for(rowOff in 0:(totalRows %/% chunkSize)) {
-      # Calculate offset to use with limit, then query based off that
-      off = rowOff * chunkSize
-      dtP = data.table::as.data.table(DBI::dbGetQuery(pCon, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
-      setorder(dtP)
+    if (isTRUE(complete)) {
+      for (rowOff in 0:(totalRows %/% chunkSize)) {
+        # Calculate offset to use with limit, then query based off that
+        off = rowOff * chunkSize
+        dtP = data.table::as.data.table(DBI::dbGetQuery(conP, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
+        setorder(dtP)
 
-      dtB = data.table::as.data.table(bq_dataset_query(bq_dataset(project, dataset), glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
-      setorder(dtB)
+        dtB = data.table::as.data.table(DBI::dbGetQuery(conB, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
+        setorder(dtB)
 
-      allEq = all.equal(dtP, dtB, check.attributes = FALSE)
+        allEq = all.equal(dtP, dtB, check.attributes = FALSE)
 
-      if(isTRUE(allEq)){
-        # warning(glue('{table} is equal.\n'))
-      } else{
-        allEqTot = FALSE
-        notEqTables = rbind(notEqTables, data.table(table = table, reason = allEq, offset = off, chunkSize = chunkSize))
-        # warning(glue('{table} is not equal: \n{allEq}\n'))
+        if (isTRUE(allEq)) {
+          # warning(glue('{table} is equal.\n'))
+        } else{
+          allEqTot = FALSE
+          notEqTables = rbind(notEqTables, data.table(table = table, reason = allEq, offset = off, chunkSize = chunkSize))
+          # warning(glue('{table} is not equal: \n{allEq}\n'))
+        }
       }
+    } else {
+      dCountB = DBI::dbGetQuery(conB, glue('SELECT count(*) as count FROM {`table`}'))
+      totalRowsB = as.integer(dCountB$count)
+
+      if(totalRows != totalRowsB){
+        allEqTot = FALSE
+        notEqTables = rbind(notEqTables, data.table(table = table, reason = glue('Different number of rows. PSQL: {totalRows} vs. BigQuery: {totalRowsB}'), offset = as.integer(NA), chunkSize = chunkSize))
+      }
+
+      for (rowOff in c(0L,(totalRows %/% chunkSize))) {
+        # Calculate offset to use with limit, then query based off that
+        off = rowOff * chunkSize
+        dtP = data.table::as.data.table(DBI::dbGetQuery(conP, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
+        setorder(dtP)
+
+        dtB = data.table::as.data.table(DBI::dbGetQuery(conB, glue('SELECT * FROM {`table`} ORDER BY {`colOrder`} LIMIT {`chunkSize`} OFFSET {`off`}')))
+        setorder(dtB)
+
+        allEq = all.equal(dtP, dtB, check.attributes = FALSE)
+
+        if (isTRUE(allEq)) {
+          # warning(glue('{table} is equal.\n'))
+        } else{
+          allEqTot = FALSE
+          notEqTables = rbind(notEqTables, data.table(table = table, reason = allEq, offset = off, chunkSize = chunkSize))
+          # warning(glue('{table} is not equal: \n{allEq}\n'))
+        }
+      }
+
     }
 
-    if(isTRUE(allEqTot)){
+    if (isTRUE(allEqTot)) {
       warning(glue('{table} is equal.\n'))
-    } else{
+    } else {
       # notEqTables = rbind(notEqTables, data.table(table = table, reason = allEq, offset = off, chunkSize = chunkSize))
       warning(glue('{table} is not equal: \n{allEq}\n'))
     }
